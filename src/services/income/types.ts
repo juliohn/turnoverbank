9c595a4038c96678d45f6d879674adad4c24949a
Juliohn <32688146+Juliohn@users.noreply.github.com>
2022-04-29 10:12:33 -0300

    adjusts types image

--- src/services/income/types.ts
+++ src/services/income/types.ts
@@ -5,5 +5,5 @@ export interface IncomesProps extends TransactionProps {}
 export type NewIncomeProps = {
   amount:string,
   description:string,
-  image_path:string | ArrayBuffer
+  image_path:string
 };
